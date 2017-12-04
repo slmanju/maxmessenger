d214df90d38d497ac9618ead18d1f6ca38eb131c
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-12-04 15:02:43 +0530

    move message impl

--- maxmessenger-api/src/main/java/com/manjula/maxmessenger/service/impl/MessageServiceImpl.java
+++ maxmessenger-api/src/main/java/com/manjula/maxmessenger/service/impl/MessageServiceImpl.java
@@ -1,8 +1,9 @@
-package com.manjula.maxmessenger.service;
+package com.manjula.maxmessenger.service.impl;
 
 import com.manjula.maxmessenger.dto.MessageDto;
 import com.manjula.maxmessenger.model.Message;
 import com.manjula.maxmessenger.repository.MessageRepository;
+import com.manjula.maxmessenger.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
