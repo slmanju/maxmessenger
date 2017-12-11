5e98e05a55432afcdd08e37e6b236925a636f8d1
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2017-12-11 16:05:34 +0530

    add email validation

--- maxmessenger-ui/src/app/users/users-edit/users-edit.component.ts
+++ maxmessenger-ui/src/app/users/users-edit/users-edit.component.ts
@@ -29,7 +29,7 @@ export class UsersEditComponent implements OnInit {
     this.userForm = this.formBuilder.group({
       firstName: new FormControl('', Validators.required),
       lastName: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', [ Validators.required, Validators.email ]),
       password: new FormControl('', Validators.required)
     });
   }
